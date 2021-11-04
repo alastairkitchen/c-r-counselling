24a70491b3cb038c6766076418c0223cf8f14b7a
alastairkitchen <ali7566@gmail.com>
2021-11-04 15:26:40 +0000

    Style footer

--- components/footer.js
+++ components/footer.js
@@ -1,33 +1,49 @@
+import Image from 'next/image'
+import SocialIcons from './socialIcons/socialIcons';
+import BacpLogo from '/Volumes/alimac/websites/c-r-counselling/public/images/accreditations/bacp-logo.png';
+import ActoLogo from '/Volumes/alimac/websites/c-r-counselling/public/images/accreditations/acto-logo.png';
 
 export default function Footer() {
 	return (
 		<footer className="site-footer">
-			<div className="container">
-				<div className="container-small">
+			<div className="container-small">
+				<div className="row">
 					<div className="site-footer__inner">
 
-						<div className="site-footer__left-col">
+						<div className="site-footer__address-col">
 							<address>
 								<p><span className="bold-font">Address:</span> Catherine Robertson Counselling, Sandyford Place, Glasgow G3 7NG</p>
 							</address>
 							<p><span className="bold-font">Hours available:</span> Monday 1pm - 5pm and Tuesday 5.30pm - 9pm</p>
 							<p><span className="bold-font">Call:</span> 07843 933 343</p>
-							<p><span className="bold-font">Email:</span> <a href="mailto:catherine@crcounselling.com">catherine@crcounselling.com</a></p>
-
-							<div>[logos]</div>
+							<p><span className="bold-font">Email:</span> <a className="anchor-link-alt" href="mailto:catherine@crcounselling.com">catherine@crcounselling.com</a></p>
+							<SocialIcons listClass="footer__social-icons" />
 							<ul className="inline-list">
 								<li><a href="#">Privacy Policy</a></li>
 								<li><a href="#">Cookies Policy</a></li>
 							</ul>
 						</div>
 
-						<div className="site-footer__right-col">
-							<ul>
-								<li><a href="#">Counselling for Adult FAQs</a></li>
-								<li><a href="#">Counselling for Children and Young People FAQs</a></li>
-								<li><a href="#">Online Counselling FAQs</a></li>
+						<div className="site-footer__links-col">
+							<ul className="site-footer__links-list remove-default-list link-list-row">
+								<li><a className="anchor-link" href="#">Counselling for Adult FAQs</a></li>
+								<li><a className="anchor-link" href="#">Counselling for Children and Young People FAQs</a></li>
+								<li><a className="anchor-link" href="#">Online Counselling FAQs</a></li>
 							</ul>
-
+							<ul className="accreditation-logos remove-default-list">
+								<li className="accreditation-logos__bacp">
+									<Image
+										src={BacpLogo}
+										alt="Bacp Logo"
+									/>
+								</li>
+								<li>
+									<Image
+										src={ActoLogo}
+										alt="Acto Logo"
+									/>
+								</li>
+							</ul>
 						</div>
 					</div>
 
