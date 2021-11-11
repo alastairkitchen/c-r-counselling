57c1fdfbb836021e8466285c57a5199d43296bb9
alastairkitchen <ali7566@gmail.com>
2021-11-11 14:14:26 +0000

    Style ul bullets, adds flipped prop to image article component, add html parser for content

--- components/imageArticle/imageArticle.js
+++ components/imageArticle/imageArticle.js
@@ -1,23 +1,24 @@
 
 import Image from 'next/image';
-import CathyHeadShot from '../../public/images/cathy-headshot-desktop.png';
+import parse from 'html-react-parser';
 
-export default function ImageArticle({ listClass }) {
+export default function ImageArticle({ listClass, flip, header, subHeader, content, image }) {
 	return (
-		<article className="image-article">
+		<article className={`image-article typography-banner ${flip ? 'image-article--flip' : ''}`}>
 			<div className="container-small">
 				<div className="row image-article__row">
 					<div className="image-article__image-col">
-						<Image
-							src={CathyHeadShot}
-							alt="Catherine Profile Shot"
-						/>
+						{Image && (
+							<Image
+								src={image}
+								alt="Catherine Profile Shot"
+							/>
+						)}
 					</div>
 					<div className="image-article__text-col">
-						<h2 className="h2">About Me</h2>
-						<p className="bold-header">My Approach</p>
-						<p>I know that being genuinely listened to and supported to see that your thoughts and feelings are valid can help you to make changes both within yourself and in your life. I won’t tell you how you should feel or what you should do. But I may offer resources that can help you deepen your understanding of yourself, your situation or the choices you can make.</p>
-						<p>Because I have completed training in more than one type of therapy, I can adapt my way of working to help clients express their feelings in a way that feels comfortable and constructive for them. My primary training is in person-centred therapy and is at the heart of whatever approach I use. Many studies show that offering this type of authentic, collaborative counselling relationship best supports people to grow, heal and discover new perspectives.</p>
+						<h2 className="h2">{header}</h2>
+						{subHeader && (<p className="bold-header">{subHeader}</p>)}
+						{content && (parse(content))}
 					</div>
 				</div>
 			</div>
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ import HeroBanner from "../components/heroBanner";
 import ContentBanner from "../components/contentBanner";
 import ImageArticle from "../components/imageArticle/imageArticle";
 import ContentBoxBanner from "../components/contentBoxBanner/contentBoxBanner";
-
+import CathyHeadShot from '../public/images/cathy-headshot-desktop.png';
+import RoomImage from '../public/images/counselling-room-setup.png';
 
 export default function Home() {
 
@@ -30,8 +31,20 @@ export default function Home() {
 
           <HeroBanner />
           <ContentBanner content={containerContent} />
-          <ImageArticle />
+          <ImageArticle
+            header='About Me'
+            subHeader='My Approach'
+            content='<p>I know that being genuinely listened to and supported to see that your thoughts and feelings are valid can help you to make changes both within yourself and in your life. I won’t tell you how you should feel or what you should do. But I may offer resources that can help you deepen your understanding of yourself, your situation or the choices you can make.</p><p>Because I have completed training in more than one type of therapy, I can adapt my way of working to help clients express their feelings in a way that feels comfortable and constructive for them. My primary training is in person-centred therapy and is at the heart of whatever approach I use. Many studies show that offering this type of authentic, collaborative counselling relationship best supports people to grow, heal and discover new perspectives.</p>'
+            image={CathyHeadShot}
+          />
           <ContentBoxBanner />
+          <ImageArticle
+            flip
+            header='Counselling for adults'
+            content='<p>Are you struggling with something that feels outside of your control?</p><p>You might be feeling overwhelmed, upset or stuck with your problems. I know asking for support with this might feel difficult. You may even feel unsure whether talking to someone will really help. It is normal to feel nervous and I will be sensitive to this when we first meet.</p><p>I will take time to create a space that feels safe for you to talk about difficult feelings or experiences you’ve had. Often people feel a great sense of relief once able to open up. Over time, you can also expect to feel more confident in yourself and in control of your life again.</p><p>The counselling I offer will focus on your needs, wants and goals. Where these may feel unclear, you will be supported to discover what these are. Whilst many people benefit from talking about their problems, we can explore if other ways of working could help you.</p><a href="#add-pdf">Download Counselling for Adults FAQs PDF</a>'
+            image={RoomImage}
+
+          />
           <div className="placeholder-counselling">[COUNSELLING]</div>
           <div className="placeholder-appointments"></div>
 
