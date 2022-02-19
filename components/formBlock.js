895dec619b726cec29110c1d6d007604b947b484
alastairkitchen <ali7566@gmail.com>
2022-02-19 13:59:16 +0000

    Refresh hush form on mount - fix for hush form init bug

--- components/formBlock.js
+++ components/formBlock.js
@@ -1,10 +1,8 @@
 
 import Script from 'next/script';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
 export default function FormBlock({ background, title }) {
-
-	const [hushLoaded, setHushLoaded] = useState(false);
 
 	const backgroundColourMap = {
 		grey: 'background-block--grey',
@@ -12,23 +10,9 @@ export default function FormBlock({ background, title }) {
 	}
 
 	useEffect(() => {
-		/*
-		// BUG figure out a way to reload hush if the target is empty
-		const hushTarget = document.getElementById('hush-form-target');
-		console.dir(hushLoaded);
-		if (hushLoaded === false && hushTarget && hushTarget.firstChild === null) {
-			const existingScript = document.getElementById('hush-forms-script');
-			const existingScriptDynamic = document.getElementById('hush-forms-script-dynamic');
-			if (existingScript) {
-				existingScript.parentNode.removeChild(existingScript);
-				const script = document.createElement('script');
-				script.src = 'https://hushforms.com/f/public/javascript/embed-hush-form.js';
-				script.id = 'hush-forms-script-dynamic';
-				document.body.appendChild(script);
-			}
-		}
-		*/
-	});
+		// refresh form when component mounts, fix for bug where form init is lost when users navigate away from homepage and back again
+		window.HUSH ? window.HUSH.embedHushForm.refresh() : null
+	}, []);
 
 	return (
 		<div className="form-block">
@@ -36,9 +20,6 @@ export default function FormBlock({ background, title }) {
 				id="hush-forms-script"
 				src="https://hushforms.com/f/public/javascript/embed-hush-form.js"
 				strategy="afterInteractive"
-				onLoad={() => {
-					setHushLoaded(true);
-				}}
 			/>
 			{title && (
 				<div className="form-block__title-wrapper">
@@ -46,7 +27,7 @@ export default function FormBlock({ background, title }) {
 				</div>
 			)}
 			<div className={`form-block__row ${backgroundColourMap[background]}`}>
-				<div data-secure-form="crcounselling" data-secure-form-transparent-background="true" id="hush-form-target"></div>
+				<div data-secure-form="crcounselling" data-secure-form-transparent-background="true"></div>
 			</div>
 		</div>
 	)
